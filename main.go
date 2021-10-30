cf97fbbc2038d2659c469c606514ebc1ce784fb7
Los-had <githubtestefinal@gmail.com>
2021-10-30 16:52:08 -0300

    GenerateURL() function created

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"github.com/gocolly/colly"
 	"bufio"
-	"strconv"
+	"fmt"
 	"os"
+	"strconv"
+	"strings"
+	"github.com/gocolly/colly"
 )
 
 func main() {
 	fmt.Println("Welcome to google search on your terminal!")
+}
+
+func GenerateURL(search_term string) string {
+	search_term = strings.Replace(search_term, " ", "+", -1)
+
+	return search_term
 }
